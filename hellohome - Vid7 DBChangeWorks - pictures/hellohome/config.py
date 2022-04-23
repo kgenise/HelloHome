f77bab9bd788bc7f47a145b19581181f00c9f166
siddiqua-aye <82129736+siddiqua-aye@users.noreply.github.com>
2022-04-22 22:55:35 -0400

    Add files via upload

---
+++ hellohome - Vid7 DBChangeWorks - pictures/hellohome/config.py
@@ -0,0 +1,11 @@
+#store database credentials
+
+host = "ec2-18-214-134-226.compute-1.amazonaws.com"
+
+port = 5432
+
+database = "d31no865tm4dj8"
+
+user = "zxkcchntuqdmgn"
+
+password = "097bdf7f9637792a36d5ec03acc859bda681f78611c2ffc180eea5e33750c174"
